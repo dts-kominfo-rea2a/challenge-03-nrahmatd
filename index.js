050d7b6e85f6edff3c346f3991b96800526143f8
nur.rahmat <nur.rahmat@aksaramaya.com>
2022-06-02 17:25:02 +0700

    Done task challange 03

--- index.js
+++ index.js
@@ -21,10 +21,22 @@ const dataBelanjaan = [
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+const listBelanjaan = (data) => {
+  let daftarBelanja = [];
+  for (let i = 0; i < data.length; ++i) {
+    daftarBelanja.push(`- ${data[i].nama} x ${data[i].kuantitas}`)
+  }
+  return daftarBelanja;
+}
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+const totalBelanjaan = (data) => {
+  let total = 0;
+  for (let i = 0; i < data.length; ++i) {
+    total += data[i].harga * data[i].kuantitas
+  }
+  return total;
+}
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
